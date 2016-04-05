3241a1a94f74e43ae42434129e0d3bb2be126327
GraemeB <grae22@gmail.com>
2016-04-05 21:29:05 +0200

    Basic maths Processor and unit-test proving circuit is processing correctly.

--- Loggel/Processors/Maths.cs
+++ Loggel/Processors/Maths.cs
@@ -1,26 +1,142 @@
 ﻿using System;
-using Loggel;
 
 namespace Loggel.Processors
 {
-  class Maths<T> : Processor<T>
+  public class Maths<T> : Processor<T>
     where T : IComparable
   {
     //-------------------------------------------------------------------------
+    // PROPERTIES.
 
-    //public enum
+    public char Operator { get; set; }
+    public T Value2 { get; set; }
+
+    //-------------------------------------------------------------------------
+    // METHODS.
+
+    public Maths()
+    {
+
+    }
 
     //-------------------------------------------------------------------------
 
     public override Processor<T> Process( Circuit<T>.CircuitContext context )
     {
+      // int.
       if( context.Value is int )
       {
-        
+        switch( Operator )
+        {
+          case '+':
+            PerformAddition_int( context );
+            break;
+
+          case '-':
+            PerformSubtraction_int( context );
+            break;
+
+          case 'x':
+            PerformMultiplication_int( context );
+            break;
+
+          case '/':
+            PerformDivision_int( context );
+            break;
+        }
+      }
+      // double.
+      else if( context.Value is double )
+      {
+        switch( Operator )
+        {
+          case '+':
+            PerformAddition_double( context );
+            break;
+
+          case '-':
+            PerformSubtraction_double( context );
+            break;
+
+          case 'x':
+            PerformMultiplication_double( context );
+            break;
+
+          case '/':
+            PerformDivision_double( context );
+            break;
+        }
       }
 
-
       return null;
+    }
+
+    //-------------------------------------------------------------------------
+    // Addition.
+
+    private void PerformAddition_int( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (int)(object)context.Value + (int)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void PerformAddition_double( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (double)(object)context.Value + (double)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+    // Subtraction.
+
+    private void PerformSubtraction_int( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (int)(object)context.Value - (int)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void PerformSubtraction_double( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (double)(object)context.Value - (double)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+    // Multiplication.
+
+    private void PerformMultiplication_int( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (int)(object)context.Value * (int)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void PerformMultiplication_double( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (double)(object)context.Value * (double)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+    // Division.
+
+    private void PerformDivision_int( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (int)(object)context.Value / (int)(object)Value2 );
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void PerformDivision_double( Circuit<T>.CircuitContext context )
+    {
+      context.Value =
+        (T)(object)( (double)(object)context.Value / (double)(object)Value2 );
     }
 
     //-------------------------------------------------------------------------
--- Loggel_Test/Circuit_Test.cs
+++ Loggel_Test/Circuit_Test.cs
@@ -8,20 +8,40 @@ namespace Loggel_Test
   public class Circuit_Test
   {
     //-------------------------------------------------------------------------
-    // Test if setting a signal's value works.
 
     [TestMethod]
-    public void ValueSet()
+    public void MathsTest()
     {
       Circuit<double> deltaTime = new Circuit<double>( 1.0 );
       Circuit<double> timeCheck = new Circuit<double>( 0.0 );
-      Router<double> processor = new Router<double>();
-      timeCheck.EntryProcessor = processor;
-      processor.Circuit_ComparisonValue = deltaTime;
+      Router<double> router = new Router<double>();
+      Maths<double> maths = new Maths<double>();
 
+      timeCheck.EntryProcessor = router;
+
+      router.Circuit_ComparisonValue = deltaTime;
+      router.OutSocket_NotEqual.ConnectedProcessor = maths;
+      router.OutSocket_Equal.ConnectedProcessor = maths;
+
+      maths.Operator = '+';
+      maths.Value2 = 1.0;
       timeCheck.Process();
+      Assert.AreEqual( timeCheck.Value, 1.0 );
 
-      //Assert.AreEqual(  )
+      maths.Operator = 'x';
+      maths.Value2 = 10.0;
+      timeCheck.Process();
+      Assert.AreEqual( timeCheck.Value, 10.0 );
+
+      maths.Operator = '/';
+      maths.Value2 = 2.0;
+      timeCheck.Process();
+      Assert.AreEqual( timeCheck.Value, 5.0 );
+
+      maths.Operator = '-';
+      maths.Value2 = 5.0;
+      timeCheck.Process();
+      Assert.AreEqual( timeCheck.Value, 0.0 );
     }
 
     //-------------------------------------------------------------------------
