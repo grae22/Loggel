79429697d4150acdd15e705e42df3e4c0c8d9d9f
GraemeB <grae22@gmail.com>
2016-04-06 23:26:54 +0200

    'Or' processor and tests.

---
+++ Loggel/Processors/Or.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Loggel.System;
+
+namespace Loggel.Processors
+{
+  public class Or : Processor
+  {
+    //-------------------------------------------------------------------------
+    // TYPES.
+
+    public class Condition
+    {
+      public Circuit Circuit { get; set; }
+      public dynamic TestValue { get; set; }
+    }
+
+    //-------------------------------------------------------------------------
+    // PROPERTIES.
+
+    // This processor's output socket.
+    public Socket OutputSocket { get; set; }
+
+    // List of conditions's which we'll 'or' when processing.
+    public List<Condition> Conditions { get; set; } = new List<Condition>();
+
+    //-------------------------------------------------------------------------
+    // METHODS.
+
+    public Or()
+    {
+      OutputSocket = AddOutputSocket( "Result", "Will be live when OR condition is satisfied." );
+    }
+
+    //-------------------------------------------------------------------------
+    // If all the conditions are met then we can return the connected
+    // processor (if there is one) as the next processor to be processed.
+
+    public override Processor Process( Circuit.CircuitContext context )
+    {
+      Processor nextProcessor = null;
+      bool anyConditionsMet = false;
+
+      // We let the signal pass through if there are no conditions.
+      if( Conditions.Count == 0 )
+      {
+        anyConditionsMet = true;
+      }
+
+      // Evaluate each condition.
+      foreach( Condition condition in Conditions )
+      {
+        if( condition.Circuit != null &&
+            condition.TestValue != null )
+        {
+          if( condition.Circuit.Value == condition.TestValue )
+          {
+            anyConditionsMet = true;
+            break;
+          }
+        }
+      }
+
+      if( anyConditionsMet )
+      {
+        nextProcessor = OutputSocket.ConnectedProcessor;
+      }
+
+      return nextProcessor;
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
---
+++ Loggel_Test/Processors/Or_Test.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Loggel.System;
+using Loggel.Processors;
+
+namespace Loggel_Test.Processors
+{
+  [TestClass]
+  public class OrProcessor
+  {
+    //-------------------------------------------------------------------------
+
+    private Circuit m_circuit;
+    private Or m_or;
+
+    //-------------------------------------------------------------------------
+
+    [TestInitialize]
+    public void Initialise()
+    {
+      Maths maths = new Maths();
+      maths.Operator = '+';
+      maths.Value2 = 1;
+
+      m_or = new Or();
+      m_or.OutputSocket.ConnectedProcessor = maths;
+
+      m_circuit = new Circuit( 0 );
+      m_circuit.EntryProcessor = m_or;
+    }
+
+    //-------------------------------------------------------------------------
+    // Test that when no conditions are specified the processor simply selects
+    // the connected processor for processing.
+
+    [TestMethod]
+    public void NoConditions()
+    {
+      m_circuit.Process();
+      Assert.AreEqual( 1, m_circuit.Value, "Signal should have passed." );
+    }
+
+    //-------------------------------------------------------------------------
+
+    [TestMethod]
+    public void ConditionsMet()
+    {
+      Or.Condition condition1 = new Or.Condition();
+      condition1.Circuit = new Circuit( 123 );
+      condition1.TestValue = 123;
+
+      Or.Condition condition2 = new Or.Condition();
+      condition2.Circuit = new Circuit( 999 );
+      condition2.TestValue = 456;
+
+      m_or.Conditions.Add( condition1 );
+      m_or.Conditions.Add( condition2 );
+      m_circuit.Process();
+      Assert.AreEqual( 1, m_circuit.Value, "Signal should have passed." );
+    }
+
+    //-------------------------------------------------------------------------
+
+    [TestMethod]
+    public void ConditionsNotMet()
+    {
+      Or.Condition condition1 = new Or.Condition();
+      condition1.Circuit = new Circuit( 999 );
+      condition1.TestValue = 123;
+
+      Or.Condition condition2 = new Or.Condition();
+      condition2.Circuit = new Circuit( 999 );
+      condition2.TestValue = 456;
+
+      m_or.Conditions.Add( condition1 );
+      m_or.Conditions.Add( condition2 );
+      m_circuit.Process();
+      Assert.AreEqual( 0, m_circuit.Value, "Signal should not have passed." );
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
