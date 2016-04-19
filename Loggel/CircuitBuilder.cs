a22dc33e6414014d6ecf50a1296ff982b0ceafc3
GraemeB <grae22@gmail.com>
2016-04-19 18:14:36 +0200

    Roadmap update for logging.

--- Loggel/CircuitBuilder.cs
+++ Loggel/CircuitBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
+using System.IO;
 
 namespace Loggel
 {
@@ -20,19 +22,40 @@ namespace Loggel
 
     //-------------------------------------------------------------------------
 
-    public static void SaveToXmlFile( string absFilename,
-                                      Circuit circuit )
+    public static void Save( string absParentDirectory,
+                             Circuit circuit )
     {
+      string circuitDir = absParentDirectory + '\\' + circuit.Name + '\\';
+
+      // Create a directory for this circuit if it doesn't already exist.
+      if( Directory.Exists( circuitDir ) == false )
+      {
+        try
+        {
+          Directory.CreateDirectory( circuitDir );
+        }
+        catch( Exception ex )
+        {
+          // TODO: Handle this.
+          throw ex;
+        }
+      }
+
+      // Create a XML doc for this circuit.
       XmlDocument doc = new XmlDocument();
       XmlElement rootElement = doc.CreateElement( "Root" );
       doc.AppendChild( rootElement );
+      circuit.GetAsXml( rootElement );
+      doc.Save( circuitDir + "Circuit.xml" );
 
       foreach( Component component in circuit.Context.Components.Values )
       {
+        doc = new XmlDocument();
+        rootElement = doc.CreateElement( "Root" );
+        doc.AppendChild( rootElement );
         component.GetAsXml( rootElement );
+        doc.Save( circuitDir + component.Name + ".xml" );
       }
-
-      doc.Save( absFilename );
     }
 
     //-------------------------------------------------------------------------
