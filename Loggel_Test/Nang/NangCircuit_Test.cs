1b315eec8d0b0f35b0b9ad4b1f85cad637592ab6
GraemeB <grae22@gmail.com>
2016-09-12 07:51:48 +0200

    Added test class.

---
+++ Loggel_Test/Nang/NangCircuit_Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Loggel.Nang;
+
+namespace Loggel_Test
+{
+  [TestClass]
+  public class NangCircuit_Test
+  {
+    //-------------------------------------------------------------------------
+
+    [TestInitialize]
+    public void Initialise()
+    {
+
+    }
+
+    //-------------------------------------------------------------------------
+
+    //[TestMethod]
+    //public void NangCircuit_()
+    //{
+
+    //}
+
+    //-------------------------------------------------------------------------
+  }
+}
